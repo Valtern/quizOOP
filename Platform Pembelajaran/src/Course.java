8d3ddbfaf733509defccba04c60bd523a1597bb2
CrushedKatana <charelsadewo123@gmail.com>
2024-09-24 16:35:36 +0700

    commit

--- Platform Pembelajaran/src/Course.java
+++ Platform Pembelajaran/src/Course.java
@@ -1,2 +1,34 @@
 public class Course {
+    private String kursusSoftware;
+    private String kursusMaketing;
+    private String kursusSeller;
+
+    Course (String kursusSoftware, String kursusMarketing, String kursusSeller ){
+        this.kursusSoftware = kursusSoftware;
+        this.kursusMaketing = kursusMarketing;
+        this.kursusSeller = kursusSeller;
+    }
+
+    public void setkursusSoftware (String kursusSoftware){
+        this.kursusSoftware = kursusSoftware;
+    }
+
+    public void setkursusMarketing (String kursusMarketing){
+        this.kursusMaketing = kursusMarketing;
+    }
+
+    public void setkursusSeller (String kursusSeller){
+        this.kursusSeller = kursusSeller;
+    }
+
+    public void print (){
+        System.out.println("Kursus Software  : " + kursusSoftware);
+        System.out.println("Kursus Marketing : " + kursusMaketing);
+        System.out.println("Kursus Seller  : " + kursusSeller);
+    }
+
+
+
+
+
 }
---
+++ Platform Pembelajaran/src/KursusSoftware.java
@@ -0,0 +1,14 @@
+public class KursusSoftware extends Course {
+
+    private String lokasiBelajar;
+    private int tanggalMengajar;
+    private int jamMengajar;
+
+    KursusSoftware(String lokasiBelajar,  int tanggalMengajar, int jamMengajar){
+        this.lokasiBelajar = lokasiBelajar;
+        this.tanggalMengajar = tanggalMengajar;
+        this.jamMengajar = jamMengajar;
+    }
+
+
+}
