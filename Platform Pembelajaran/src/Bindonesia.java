80044bb4ae1f091cfb55608b629377c43de7e942
CrushedKatana <charelsadewo123@gmail.com>
2024-09-24 16:47:29 +0700

    commit

---
+++ Platform Pembelajaran/src/Bindonesia.java
@@ -0,0 +1,14 @@
+public class Bindonesia {
+
+    private String lokasiBelajar;
+    private int tanggalMengajar;
+    private int jamMengajar;
+
+    Bindonesia (String lokasiBelajar,  int tanggalMengajar, int jamMengajar){
+        this.lokasiBelajar = lokasiBelajar;
+        this.tanggalMengajar = tanggalMengajar;
+        this.jamMengajar = jamMengajar;
+    }
+
+
+}
---
+++ Platform Pembelajaran/src/Mathematic.java
@@ -0,0 +1,14 @@
+public class Mathematic {
+    
+    private String lokasiBelajar;
+    private int tanggalMengajar;
+    private int jamMengajar;
+
+    Mathematic(String lokasiBelajar,  int tanggalMengajar, int jamMengajar){
+        this.lokasiBelajar = lokasiBelajar;
+        this.tanggalMengajar = tanggalMengajar;
+        this.jamMengajar = jamMengajar;
+    }
+
+
+}
